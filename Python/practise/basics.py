ec42cb8f109bab42aa5aa257038e7fca813a39f3
Ayush-141196 <asgpnm11@gmail.com>
2026-04-20 17:52:52 +0530

    new python notes & practise added

--- Python/practise/basics.py
+++ Python/practise/basics.py
@@ -14,3 +14,46 @@ print(d)
 e = 'amansingh'
 print(e)
 
+
+print(1)
+print("xyz12344")
+print("abc123")
+
+f="xyz"
+print(f)
+
+# for comment out we use "#"
+g = "ayush here for learning python"
+print(g)
+
+# Variable & constants
+a = 100
+b = 200
+# (a & b are variables) and (100 & 200 are constants) and "=" is assignement operator.
+print(a+b)
+print(b+a)
+
+#  + - * / = these all are operators
+# a + b , a or b is variables, + is operators , both together called as OPERANDS
+#  a + b allt his together is called as operations
+
+z = 40
+y = 20
+x = z * y #(Operations)
+#print("the multiplication of z * y is", x)
+print(type(y))
+
+# Data Type
+
+# Integar 1 20 654 7987 -1 87 14
+# Boolean True or False
+# floating number 1.2 2.3 5.9 8.1 
+# string "this is string"
+
+xx = 1
+print(xx)
+
+# if you run python3 in terminal you will go to Interactiveshell
+# where you can do REPL -Read Evaluate Print Loop
+# in case your file is not Workin you can do practice on interactive shell.
+
---
+++ Python/practise/user_env.py
@@ -0,0 +1,18 @@
+env = input("enter the env: ")
+print("the user input env is :",env)
+
+# Conditional Statement
+if env == "test":
+    print("Take backup")
+elif env == "prod":
+    print("Don't Deploy on Friday")
+else:
+    print("Safe to Deploy Any day")
+    
+# type casting - conversion of one datatype to another
+a = int(input("enter the no 1: "))
+b = int(input(" Enter the no 2: "))
+print("addition of 1 & 2:", a + b)
+print("subtraction of 1 & 2:", a - b)
+print("multiplication of 1 & 2:", a * b)
+print("dividation of 1 & 2:", a / b)
